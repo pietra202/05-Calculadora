7e20491efc47c324de23696c751ce6066f6a4d00
Pietra <pgodoifelipe@gmail.com>
2025-09-10 10:33:01 -0300

    mudanças

--- App.js
+++ App.js
@@ -1,77 +1,187 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
- const [display, setDisplay] = useState('0');
- const [operador, setOperador] = useState(null);
- const [firstValue, setFirstValue] = useState(null);
+  const [valorDigitado, setValorDigitado] = useState('');
+  const [resultado, setResultado] = useState('');
 
- const handleInput = (input) => {
-  if (['+', '-', 'x', '%'].includes(input)) {
-    setOperador(input);
-    setFirstValue(display);
-    setDisplay('0');
-  } else if (input === '=') {
-    if (operador && firstValue !== null) {
-      const a = parseFloat(firstValue);
-      const b = parseFloat(display);
-      let resut = 0;
-      switch (operador) {
-        case '+': result = a + b; break;
-        case '-': result = a - b; break;
-        case 'x': result = a * b; break;
-        case '%': result = a === 0 ? 'Erro' : a / b; break;
-        default: break;
-      }
+  const handlePress = (value) => {
+    setValorDigitado((prev) => prev + value);
+  };
 
-      setDisplay(String(result));
-      setOperador(null);
-      setFirstValue(null);
+  const igual = () => {
+    try {
+      const calcResult = eval(valorDigitado);
+      setResultado(valorDigitado);
+      setValorDigitado(calcResult.toString());
+    } catch (error) {
+      setResultado('Erro');
     }
-  } else if (input === 'C') {
-    setDisplay('0');
-    setOperador(null);
-    setFirstValue(null);
-  } else {
-    setDisplay((prev) => (prev === '0' ? input : prev + input));
-  }
- };
+  };
 
- const button = ({label}) => (
-  <TouchableOpacity style={style.btn} onPress={() => handleInput(label)}>
+  const handleClear = () => {
+    setResultado('');
+    setValorDigitado('');
+  };
 
-  </TouchableOpacity>
- );
+  return (
+    <View style={styles.container}>
+      <View style={styles.display}>
+        <Text style={styles.resultText}>{resultado}</Text>
+        <Text style={styles.inputText}>{valorDigitado}</Text>
+      </View>
 
- return (
-  <SafeAreaView style={StyleSheet.container}>
-    <View style={StyleSheet.displayArea}>
-      <Text style={style.display}>{display}</Text>
+      {/* botões da calculadora */}
+      <View style={styles.buttons}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('7')}>
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('8')}>
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('9')}>
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress(' / ')}>
+            <Text style={styles.buttonText}>÷</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('4')}>
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('5')}>
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('6')}>
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress(' * ')}>
+            <Text style={styles.buttonText}>×</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('1')}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('2')}>
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('3')}>
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress(' - ')}>
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={handleClear}>
+            <Text style={styles.buttonText}>C</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('0')}>
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress('.')}>
+            <Text style={styles.buttonText}>,</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handlePress(' + ')}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={igual}>
+            <Text style={styles.buttonText}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
-    <view style={styles.row}>
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="%" />
-      </view>
-    <view style={styles.row}>
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="x" />
-    </view>
-      <view style={styles.row}>
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="-" />
-    </view>
-     <view style={styles.row}>
-      <Button label="0" />
-      <Button label="C" />
-      <Button label="=" />
-      <Button label="+" />
-    </view>
-  </SafeAreaView>
- 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 20,
+    backgroundColor: '#222',
+  },
+  display: {
+    marginBottom: 20,
+    padding: 20,
+    minHeight: 100,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    borderRadius: 10,
+    backgroundColor: '#333',
+  },
+  inputText: {
+    fontSize: 28,
+    color: '#fff',
+  },
+  resultText: {
+    fontSize: 24,
+    color: '#aaa',
+    fontWeight: 'bold',
+  },
+  buttons: {
+    marginBottom: 40,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    margin: 5,
+    paddingVertical: 20,
+    borderRadius: 10,
+    backgroundColor: '#444',
+  },
+  buttonText: {
+    fontSize: 24,
+    color: '#fff',
+  },
+});
